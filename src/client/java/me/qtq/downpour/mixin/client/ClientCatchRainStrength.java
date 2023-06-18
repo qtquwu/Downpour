60cad287ede9edebc128d3f673d10d4b263e3279
Quinn <quinn.t.quinley@gmail.com>
2023-06-18 03:03:40 -0500

    Fix for invisible rain glitch

--- src/client/java/me/qtq/downpour/mixin/client/ClientCatchRainStrength.java
+++ src/client/java/me/qtq/downpour/mixin/client/ClientCatchRainStrength.java
@@ -1,5 +1,7 @@
 package me.qtq.downpour.mixin.client;
 
+import me.qtq.downpour.Downpour;
+import me.qtq.downpour.ILocalRainClient;
 import me.qtq.downpour.IRainable;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
@@ -8,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
@@ -15,21 +18,24 @@ public class ClientCatchRainStrength {
 	@Shadow
 	private ClientWorld world;
 
-	@Inject(at = @At("HEAD"), method = "onGameStateChange")
+	@Inject(at = @At("TAIL"), method = "onGameStateChange")
 	private void catchRainStrength(GameStateChangeS2CPacket packet, CallbackInfo info) {
 		if (world == null)
 			return;
 		if (packet.getReason() == GameStateChangeS2CPacket.RAIN_STARTED) {
-			// if the packet value is equal to zero, the server is unmodded. Assume maximum strength.
-			if (packet.getValue() != 0.0f) {
-				((IRainable) world).setRainStrength(packet.getValue());
-			} else {
-				((IRainable) world).setRainStrength(1.0f);
+			// If it is already raining, the game is adjusting its strength rather than starting a new storm
+			if (world.isRaining() || world.getRainGradient(1.0f) > 0.0f) {
+				world.setRainGradient(1.0f);
 			}
+			// if the packet value is equal to zero, the server is unmodded. Assume maximum strength.
+			((IRainable) world).setRainStrength(packet.getValue() != 0.0 ? packet.getValue() : 0.0f);
 		} else if (packet.getReason() == GameStateChangeS2CPacket.RAIN_STOPPED) {
 			((IRainable) world).setRainStrength(0.0f);
 		} else if (packet.getReason() == GameStateChangeS2CPacket.RAIN_GRADIENT_CHANGED) {
 			world.setRainGradient(packet.getValue());
 		}
+	}
+	@Redirect(method = "onGameStateChange", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;setRainGradient(F)V"))
+	private void cancelSetRainGradient(ClientWorld instance, float v) {
 	}
 }
